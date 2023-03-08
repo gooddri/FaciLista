e5097ca04c201d824abdf86cd37955b579cc1ab4
Jonas sousa <jonassousagtr@gmail.com>
2023-03-07 22:25:59 -0300

    Refactor: retirando algumas funcionalidades que não estavam 100% e adicionando a função de o item passar para a lista de 'comprados'.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let listaDeItens = []
 let itemAEditar
 
-
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
 const ulItens = document.getElementById("lista-de-itens")
+const ulItensComprados = document.getElementById('itens-comprados')
 const listaRecuperada = localStorage.getItem('listaDeItens')
 
 const atualizaLocalStorage = () => {
@@ -45,23 +45,36 @@ const salvarItem = () => {
 
 function exibirItens() {
         ulItens.innerHTML = ''
-    
+        ulItensComprados.innerHTML = ''
         
         listaDeItens.forEach((elemento, index) => {
-            ulItens.innerHTML += `
+            if(elemento.checar) {
+                ulItensComprados.innerHTML += `
+                    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                        <div>
+                            <input type="checkbox" checked class="is-clickable" />
+                            <span class="itens-comprados is-size-5">${elemento.valor}</span>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-trash is-clickable deletar"></i>
+                        </div>
+                    </li>
+                `
+            }else {
+                ulItens.innerHTML += `
                     <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
                         <div>
                             <input type="checkbox" class="is-clickable" />
                             <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemAEditar) ? 'disabled' : ''} ></input>
                         </div>
                         <div>
-                            ${ index === Number(itemAEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
                             <i class="fa-solid fa-trash is-clickable deletar"></i>
                         </div>
                         
                     </li>    
                     
                     `
+            }
         
         })
 
@@ -88,28 +101,10 @@ function exibirItens() {
         })
     })
 
-    const editarItens = document.querySelectorAll('.editar')
 
-    editarItens.forEach( (i) => {
-        i.addEventListener('click', (evento) => {
-            itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value')
-            exibirItens()
-            
-        })
-    })
 
 atualizaLocalStorage()
 
 }
 
-const salvarEdicao = () => {
-    let itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
-    listaDeItens[itemAEditar].valor = itemEditado.value
-    itemAEditar = -1
-    exibirItens()
-}
 
-
-
-
-   
